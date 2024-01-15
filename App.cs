bbce9b72fb62dd872b2b250facefa87a0eb2d66e
marcelldac <dacmarcell@gmail.com>
2024-01-15 11:57:45 -0300

    feat: human resources class

---
+++ App.cs
@@ -0,0 +1,27 @@
+﻿namespace App{
+    public class HumanResources{
+        string[] EmployeesList = new string[4];
+        public HumanResources(){
+            EmployeesList[0] = "Marcell";
+            EmployeesList[1] = "Natália";
+            EmployeesList[2] = "Django";
+            EmployeesList[3] = "Patrício";
+        }
+        public void ShowNames(){
+            foreach(string name in EmployeesList){
+                Console.WriteLine(name);
+            }
+        }
+
+        public void NewEmployee(string employeeName){
+            EmployeesList.Append(employeeName);
+        }
+
+        public void FireEmployee(string employeeName){
+            int indexToRemove = Array.IndexOf(EmployeesList, employeeName);
+            if(indexToRemove == -1){
+                throw new InvalidOperationException("Array Index Not Found.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+namespace App{
+    public class Program{
+        public static void Main(){
+            var recursos = new HumanResources();
+            //recursos.ShowNames();
+            recursos.FireEmployee("Patrício");
+        }
+    }
+}
